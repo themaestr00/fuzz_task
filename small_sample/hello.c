3a0bf94178faa0dcfae542d670506e122b027e67
themaestr00 <chekalin_2005@mail.ru>
2025-01-22 23:09:37 +0300

    fuzzing with a small sample

---
+++ small_sample/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(void)
+{
+    int x, a, b;
+    if (scanf("%d%d%d", &x, &a, &b) != 3) {
+        abort();
+    }
+    if (a <= x && x <= b) {
+        printf("Hello world!\n"); // If x is in [a, b] then prints "Hello world!"
+    } else {
+        abort(); // Else program is terminated with SIGABRT
+    }
+    return 0;
+}
